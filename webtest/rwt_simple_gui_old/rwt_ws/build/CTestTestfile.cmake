4c6e61eaaae638b5e82a60577cdbc1b277c83753
lianbo2006 <513748889@qq.com>
2017-03-28 22:38:10 +0800

    edit

---
+++ webtest/rwt_simple_gui_old/rwt_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lianbo2006/rwt_ws/src
+# Build directory: /home/lianbo2006/rwt_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
